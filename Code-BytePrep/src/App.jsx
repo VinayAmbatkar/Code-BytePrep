cd0299c1c59533811280e320dc7a0e1b5edac4f6
VinayAmbatkar <Vinayambatkar57@gmail.com>
2024-07-23 20:36:42 +0530

    Nav and hero img updated

--- Code-BytePrep/src/App.jsx
+++ Code-BytePrep/src/App.jsx
@@ -1,25 +1,38 @@
 import React from 'react'
 import './App.css'
+import logo from '../src/assets/Images/logo/Logo.png'
+import side from '../src/assets/Images/Hero/Side.png'
+
 
 function App() {
 
   return (
     <>
-
-      <div>
-        <div className="nav">
-          <h1>Code-BytePrep</h1>
-          <nav>
-            <ul>
-              <li><a href="#">Link 1</a></li>
-              <li><a href="#">Link 2</a></li>
-              <li><a href="#">Link 3</a></li>
-              <li><a href="#">Link 4</a></li>
-            </ul>
-          </nav>
-          <input type="text" placeholder="Search" />
+<div className="container">
+<div className="flex items-center p-4 ">
+      <img src={logo} alt="logo" className="h-10 w-auto mr-6" />
+      <div className="flex-grow">
+        <div className="relative">
+          <input type="text" placeholder="Search Hear..." />
         </div>
       </div>
+      <div className="nva-right">
+        <ol className="flex space-x-20 ml-0 text-gray-700">
+        <li><a href="#practice" className="hover:text-gray-900">Practice</a></li>
+        <li><a href="#compete" className="hover:text-gray-900">Compete</a></li>
+        <li><a href="#explore" className="hover:text-gray-900">Explore</a></li>
+        <li><a href="#login" className="text-purple-500 hover:text-purple-700">Login</a></li>
+      </ol>
+      </div>
+      
+      <button className="ml-10 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-full shadow-md ">Sign Up</button>
+    </div>
+</div>
+
+
+<img src={side} alt="" className='sidehero' />
+
+
     </>
   );
 }
